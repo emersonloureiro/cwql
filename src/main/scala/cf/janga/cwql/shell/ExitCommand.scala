2a572855960a78971408a92d5ac6b6b49d21d0e3
Emerson Loureiro <loureie@amazon.com>
2018-03-31 10:54:29 +0100

    Fixing exit status code

--- src/main/scala/cf/janga/cwql/shell/ExitCommand.scala
+++ src/main/scala/cf/janga/cwql/shell/ExitCommand.scala
@@ -3,7 +3,7 @@ package cf.janga.cwql.shell
 case class Exit(console: Console) extends Command {
   override def run(): Unit = {
     console.writeln("Exiting...")
-    System.exit(1)
+    System.exit(0)
   }
 }
 
